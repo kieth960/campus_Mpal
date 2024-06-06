8f0a390ce276c990d8734763a5154fe8a086c6a9
kieth960 <157789962+kieth960@users.noreply.github.com>
2024-06-06 15:44:40 +0200

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,4 @@
-const geminiApiKey = 'AIzaSyCMAi7mrNfu4mYT-r6yqGHlhbruKaRB1tc';
+const geminiApiKey = '';
 
 const kChatbotPromt =
     'You are expert chef, and you have a lot of knowledge about cooking. You are chatting with a user. If user ask about recipe then, make sure to ask for the necessary details like the type of nutrients, make sure to only give information about recipe nutrients';
